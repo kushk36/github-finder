ccb28f50615cd32a8dc92822710bf328e44f57d7
Kush Patel <kushpatel36@gmail.com>
2023-06-17 23:37:26 +0530

    Navbar component

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { BrowserRouter as Router } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+
+function App() {
+  return (
+    <Router>
+      <div className="flex flex-col justify-between h-screen">
+        <Navbar />
+        <main>
+          content
+        </main>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
